49cb1f19ee92225b65158a39cc9bdb785d34d0ed
Iwansal64 <iwansal64@gmail.com>
2024-11-21 14:23:17 +0700

    Adding grid for the buttons, and wallpaper, and tidy up a bit

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,6 +1,10 @@
 import { Link, useNavigate } from 'react-router-dom';
+import '../styles/Global.css';
+import '../styles/Title.css';
 import '../styles/ButtonStyles.css';
 import '../styles/LinkStyles.css';
+import '../styles/GridStyle.css';
+import '../styles/Background.css';
 
 function Homepage() {
   const navigate = useNavigate();
@@ -14,15 +18,26 @@ function Homepage() {
 
   return (
     <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to the Fitness Advisor !</h1>
-        <div className="nav-container">
-            <Link to="/user" className="link-button data-button">
-              My Data
-            </Link>
-            <Link to="/calendar" className="link-button calendar-button">
-              My Calendar
-            </Link>
-        </div>
+      <div className="bg-homepage"></div>
+      <h1 className='title'>Welcome to the Fitness Advisor !</h1>
+      <div className='grid-buttons'>
+        <Link to="/user" className="link-button data-button" title="Change your data and regenerate result!">
+          <h1>
+            My Data
+          </h1>
+          <p>
+            Customize your data, and we will figure out what you need!
+          </p>
+        </Link>
+        <Link to="/calendar" className="link-button calendar-button" title="Know what your plan for today!">
+          <h1>
+            My Calendar
+          </h1>
+          <p>
+            Open Calendar, and see what are activities you can do right now!
+          </p>
+        </Link>
+      </div>
       
       <Link to="/logout" className="logout-button">
         Logout
