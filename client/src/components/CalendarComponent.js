544b0da2ed3bcb9ca3f79679fa3b7ac45e8130d9
iwansal64 <iwansal64@gmail.com>
2024-11-30 15:19:42 +0000

    Basic Styling ReactCalendar...
    I will work on this later! At least it looks not too bad now :)

--- client/src/components/CalendarComponent.js
+++ client/src/components/CalendarComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../styles/ReactCalendar.css';
 import { Link } from 'react-router-dom';
 
 const localizer = momentLocalizer(moment);
